c8b52a03f43a3638c606fa64f322f4c813ffbc02
Adriano Santos <sleipnir@bsd.com.br>
2019-11-27 10:54:49 -0300

    Add cloudstate cli brew formula

--- cloudstate-cli.rb
+++ cloudstate-cli.rb
@@ -2,14 +2,14 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CloudstateCli < Formula
-    desc "loudState CLI"
-    homepage ""
-    url "https://github.com/sleipnir/homebrew-cloudstate-cli/archive/0.3.54.tar.gz"
-    sha256 "baf924e8aa265de9ed7b183747dd6cc3874e5e02add165f93b431db18ec74850"
-    
-    def install
-      bin.install cloudstate
-    end
+  desc "Cloudstate CLI"
+  homepage "https://cloudstate.io/"
+  url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.3.54/cloudstate-0.3.54-osx.tar.gz"
+  sha256 "c46f8abc9383bd43405cf1ccb3f7956bd0098748e344b40e5caeaeeff8d9a9ab"
 
+  def install
+    system "cp", "cloudstate",  bin/"cloudstate"
   end
+
+end
   
