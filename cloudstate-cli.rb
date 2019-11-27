3bf5439f6d01067ebcfef8c239248e3e764ec97d
Adriano Santos <sleipnir@bsd.com.br>
2019-11-27 13:06:42 -0300

    Add new url

--- cloudstate-cli.rb
+++ cloudstate-cli.rb
@@ -1,6 +1,6 @@
 class CloudstateCli < Formula
   desc "CloudState CLI Is a lightweight, fast, and fun client that lets you go from zero to production with CloudState Stateful Serveless Functions Platform in minutes."
-  homepage "https://cloudstate.io/"
+  homepage "https://github.com/sleipnir/cloudstate-cli"
   url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.3.54/cloudstate-0.3.54-osx.tar.gz"
   sha256 "c46f8abc9383bd43405cf1ccb3f7956bd0098748e344b40e5caeaeeff8d9a9ab"
 
