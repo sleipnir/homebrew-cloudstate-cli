1ab123fe2e973055d6a87d98cc7d5e7ff908dd9f
Adriano Santos <sleipnir@bsd.com.br>
2019-12-02 15:25:35 -0300

    Release 0.4.1

--- cloudstate-cli.rb
+++ cloudstate-cli.rb
@@ -1,8 +1,8 @@
 class CloudstateCli < Formula
   desc "CloudState CLI Is a lightweight, fast, and fun client that lets you go from zero to production with CloudState Stateful Serveless Functions Platform in minutes."
   homepage "https://github.com/sleipnir/cloudstate-cli"
-  url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.3.54/cloudstate-0.4.0-osx.tar.gz"
-  sha256 "c46f8abc9383bd43405cf1ccb3f7956bd0098748e344b40e5caeaeeff8d9a9ab"
+  url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.4.1/cloudstate-0.4.1-osx.tar.gz"
+  sha256 "670917342415bbcf5f70e9170c611804927f99d0bdb43c1fcf353bdf6339baf7"
 
   def install
     bin.install "release/cloudstate"
