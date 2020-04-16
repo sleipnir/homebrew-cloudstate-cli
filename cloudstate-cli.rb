caa31c4838c9d47dc17263876a7ded6e74bb5445
Adriano Santos <sleipnir@bsd.com.br>
2020-04-16 17:11:16 -0300

    New Homebrew tap of Cloudstate CLI

--- cloudstate-cli.rb
+++ cloudstate-cli.rb
@@ -1,8 +1,8 @@
 class CloudstateCli < Formula
   desc "CloudState CLI Is a lightweight, fast, and fun client that lets you go from zero to production with CloudState Stateful Serveless Functions Platform in minutes."
   homepage "https://github.com/sleipnir/cloudstate-cli"
-  url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.4.1/cloudstate-0.4.1-osx.tar.gz"
-  sha256 "670917342415bbcf5f70e9170c611804927f99d0bdb43c1fcf353bdf6339baf7"
+  url "https://github.com/sleipnir/cloudstate-cli/releases/download/0.5.4/cloudstate-0.5.4-osx.tar.gz"
+  sha256 "e914df23e30bd8ab62d13f5254ee1e5a80d86d7f705947c96d293c8c4bab50f7"
 
   def install
     bin.install "release/cloudstate"
